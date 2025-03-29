fe9d393d4162820f86347c5680bf2cd893e37feb
Игорь <112966801+SmallUpdate@users.noreply.github.com>
2025-03-29 05:02:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,68 @@
-let sum = 3,
-    x = 0,
-    s = '',
-    first = true;
-    text = document.querySelector('.text'),
-    buttons = document.querySelectorAll('.button');
 
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function generate() {
-    sum = 0;
-    s = '';
-    first = true;
-    for (let i = 1; i <= 2; i++) {
-        if (i != 2) {
-            if (sum > 0 & sum < 3) {
-                x = random(0, 1);
-                if (x = 0) {
-                    x = random(-3, -1);
-                } else {
-                    x = random(1, 3);
-                }
-            } else {
-                if (sum < 1) {
-                    x = random(1, 3);
-                } else if (sum > 2) {
-                    x = random(-3, -1);
-                }
-            }
-        } else {
-            if (sum == 0) {
-                x = random(1, 3);
-            } else if (sum < 0) {
-                x = random(Math.abs(sum - 1), 3);
-            } else if (sum > 3) {
-                x = random(-3, -(sum - 2));
-            } else if (sum == 1) {
-                x = random(1, 2);
-            } else if (sum == 3) {
-                x = random(-2, -1);
-            } else {
-                x = random(0, 1);
-                if (x == 0) {
-                    x = -1;
-                } else if (x == 1) {
-                    x = 1;
-                }
-            }
+function checkAnswer(event) {
+    if (event.target.innerHTML == a) {
+        a = random(1, 3)
+        if (l < 6) {
+            q++
+            l = Math.floor(Math.log2(q))
         }
-        sum += x;
-        if (first) {
-            s += x;
-            first = false;
-        } else if (x < 0) {
-            s += x;
-        } else if (x > 0) {
-            s += '+' + x;
-        }
+        generate()
+        timer.classList.remove('timer')
+        console.log('Correct')
+    } else {
+        console.log('Wrong')
+        
     }
-    text.innerHTML = s + '=?';
-    console.log(sum);
 }
 
-for (let i = 0; i < 3; i++) {
-    buttons[i].addEventListener('click', function () {
-        if (buttons[i].innerHTML == sum) {
-            generate()
+function generate() {
+    if (l === 2) {
+        s = v[a][random(0, 1)];
+    } else {
+        s = v[a][random(2, 3)];
+    }
+    for (let i = l; i > 2; i--) {
+        m = parseInt(s[s.length - 1])
+        if (m === 0) {
+            rnd = random(0, 2)
+        } else {
+            if (i === 3) {
+                rnd = random(0, 1)
+            } else {
+                rnd = random(2, 3)
+            }
         }
-    });
+        p = v[m][rnd]
+        if (s[s.length - 2] === '-') {
+            if (v[m][rnd][1] === '-') {
+                p = v[m][rnd].replace('-', '+')
+            } else {
+                p = v[m][rnd].replace('+', '-')
+            }
+        }
+        s = s.slice(0, -1) + p;
+    }
+    equation.innerHTML = s + '=?'
 }
+
+let equation = document.querySelector('.equation')
+const buttons = document.querySelectorAll('.button')
+const timer = document.querySelector('hr')
+for (let i = 0; i < 3; i++) {
+    buttons[i].addEventListener('click', checkAnswer)
+}
+
+const v = [
+    ['1-1', '2-2', '3-3'],
+    ['2-1', '3-2', '1+0', '1-0'],
+    ['1+1', '3-1', '2+0', '2-0'],
+    ['1+2', '2+1', '3+0', '3-0']
+];
+
+let l = 2;
+let a = 3;
+let q = 4;
